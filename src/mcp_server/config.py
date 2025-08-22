b4bfa80dace736332c3ca0512aa7ebc0126acb8c
yeeunbak <yeeunb02@gmail.com>
2025-08-22 13:21:06 +0900

    Available GMAIL API

--- src/mcp_server/config.py
+++ src/mcp_server/config.py
@@ -15,3 +15,8 @@ GMAIL_TOKEN_PATH = os.getenv("GMAIL_TOKEN_PATH", "storage/gmail_token.json")
 # Scopes
 GCAL_SCOPES = ["https://www.googleapis.com/auth/calendar.events"]
 GMAIL_SCOPES = ["https://www.googleapis.com/auth/gmail.compose"]
+# (나중에 '보내기'까지 할 거면 추가)
+# GMAIL_SCOPES = [
+#   "https://www.googleapis.com/auth/gmail.compose",
+#   "https://www.googleapis.com/auth/gmail.send",
+# ]
